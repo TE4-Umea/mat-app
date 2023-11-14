be38f0f0f443a4c461f91775ae3594ed186cff14
timnilsson04 <timnils04@gmail.com>
2023-11-14 13:00:11 +0100

    ✨

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <Header />
+        <Footer />
+      </body>
     </html>
   )
 }
