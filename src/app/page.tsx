4f059e3becbc8cea434ef70e3a0d0e45da65f8f1
= <=>
2023-11-14 13:20:58 +0100

    knas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Layout from './layout'
 
 export default function Home() {
   return (
-    <main>
-      <div className={styles.test}>
-        <p>Måltider</p>
-        <p>Maträtter</p>
-        <p>Profil</p>
-      </div>
-    </main>
+    <Layout>
+      <main>
+        <div className={styles.StartText}>
+          Välkommen tillbaka REDACTED
+        </div>
+        <div className={styles.StartChoice}>
+          <div className={styles.StartChoiceText}>
+            <p>Måltider</p>
+          </div>
+          <div className={styles.StartChoiceText}>
+            <p>Maträtter</p>
+          </div>
+          <div className={styles.StartChoiceText}>
+            <p>Profil</p>
+          </div>
+        </div>
+      </main>
+    </Layout>
   )
 }
