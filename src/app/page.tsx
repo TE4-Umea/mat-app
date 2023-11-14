4f22c1a93e94c8248dcab788ece59f79d3e521ed
= <=>
2023-11-14 13:45:42 +0100

    noice css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,18 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-      <main>
-        <div className={styles.StartText}>
-          Välkommen tillbaka REDACTED
+    <main>
+      <div className={styles.StartText}>
+        Välkommen tillbaka REDACTED
+      </div>
+      <div className={styles.StartChoice}>
+        <div className={styles.StartChoiceText}>
+          <p>Måltider</p>
         </div>
-        <div className={styles.StartChoice}>
-          <div className={styles.StartChoiceText}>
-            <p>Måltider</p>
-          </div>
-          <div className={styles.StartChoiceText}>
-            <p>Maträtter</p>
-          </div>
-          <div className={styles.StartChoiceText}>
-            <p>Profil</p>
-          </div>
+        <div className={styles.StartChoiceText}>
+          <p>Maträtter</p>
         </div>
-      </main>
+      </div>
+    </main>
   )
 }
