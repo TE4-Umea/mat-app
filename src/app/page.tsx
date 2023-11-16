a3d5aa70e3db0a9eadc8f22484e9a7405a942bdf
timnilsson04 <timnils04@gmail.com>
2023-11-16 11:21:07 +0100

    ✨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,32 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import NavBar from './components/navBar'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
-      <div className={styles.StartText}>
-        Välkommen tillbaka REDACTED
+      <div className={styles.startText}>
+        God morgon,
+        Kerstkristina! Idag ska du äta
       </div>
-      <Link
-        href="/"
-      //   className=
-      >
-        <span className="">Sparade rätter</span>{' '}
-      </Link>
-      <Link
-        href="/"
-      //   className=
-      >
-        <span className="">Favoritiserade rätter</span>{' '}
-      </Link>
+
+      {/* Scroll funktion med bilder på mat */}
+
+      <div className={styles.buttonsSaved}>
+        <Link
+          href="/"
+        //   className=
+        >
+          <span className="">Sparade rätter</span>{' '}
+        </Link>
+        <Link
+          href="/"
+        //   className=
+        >
+          <span className="">Favoritiserade rätter</span>{' '}
+        </Link>
+      </div>
       <NavBar />
     </main>
   )
