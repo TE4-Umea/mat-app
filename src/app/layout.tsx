5334f41e7f42f8d94f6a00e350a257e792869141
= <=>
2023-11-21 09:07:29 +0100

    Add Head component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Lexend_Deca, Karla } from 'next/font/google';
 import './globals.css'
+import Head from 'next/head';
 
 import { useTranslation } from 'next-i18next';
 
