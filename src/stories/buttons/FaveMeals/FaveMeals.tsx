448d518aa769855d62f2cece6ad313eb69c6d662
Angelica Sjödin <angelic.sjodin@gmail.com>
2023-11-21 10:24:33 +0100

    ✨ knapp

---
+++ src/stories/buttons/FaveMeals/FaveMeals.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './FaveMeals.css';
+import '/src/app/globals.css';
+
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+export const Button = ({
+
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={['storybook-button--FaveMeals'].join(' ')}
+      {...props}
+    >
+      {label}
+      <style jsx>{`
+        button {
+          background-color: ${backgroundColor};
+        }
+      `}</style>
+    </button>
+  );
+};
