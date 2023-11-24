6ce30f74b7bd6ae590660eebb7c012c074105903
timnilsson04 <timnils04@gmail.com>
2023-11-24 09:51:26 +0100

    🐛

--- src/app/components/timeDepender/timeDependerMiddag.jsx
+++ src/app/components/timeDepender/timeDependerMiddag.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from '../page.module.css'
+import styles from '../../page.module.css'
 
 export default function timeDependerMiddag() {
     var today = new Date();
