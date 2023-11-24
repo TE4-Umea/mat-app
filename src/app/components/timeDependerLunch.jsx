d07b7597779616a092e0f5ad397300e93e8abc66
timnilsson04 <timnils04@gmail.com>
2023-11-24 09:08:46 +0100

    ✨

--- src/app/components/timeDependerLunch.jsx
+++ src/app/components/timeDependerLunch.jsx
@@ -4,16 +4,16 @@ import styles from '../page.module.css'
 export default function timeDependerLunch() {
     var today = new Date();
 
-    if (today.getHours() < 15) {
-        return (
-            <div className={styles.primaryButtonTime}>
-                <a href="">Lunch</a>
-            </div>
-        );
-    } else if (today.getHours() > 16) {
+    if (today.getHours() <= 15) {
         return (
             <div className={styles.secondaryButtonTime}>
                 <a href="">Middag</a>
+            </div>
+        );
+    } else if (today.getHours() >= 16) {
+        return (
+            <div className={styles.secondaryButtonTime}>
+                <a href="">Lunch</a>
             </div>
         );
     } else {
