6ec3294b771759b2bd9a0f72239ef0f4e60da1b5
timnilsson04 <timnils04@gmail.com>
2023-11-27 10:15:03 +0100

    Add BackgroundTime component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={[lexendDeca.variable, karla.className].join(" ")}>
+        <BackgroundTime></BackgroundTime>
         {children}
       </body>
     </html>
