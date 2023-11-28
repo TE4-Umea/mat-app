c32c33f838f410a20cbe29a9ca58f2d46f460668
Angelica Sjödin <angelic.sjodin@gmail.com>
2023-11-28 09:26:26 +0100

    ✨ history thingie

--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 
 import { useTranslation } from 'next-i18next';
 import { Button as NavBar } from '../components/storyBookComponents/navbar/navBar';
+import { Button as Background} from '../components/storyBookComponents/background/background'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Background></Background>
         {children}
         <NavBar />
       </body>
