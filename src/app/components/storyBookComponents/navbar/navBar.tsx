66b3b1dfe44686c1c95b16081ce46c8f06cc4ff3
timnilsson04 <timnils04@gmail.com>
2023-11-28 14:35:51 +0100

    Fix active class styling in navbar

--- src/app/components/storyBookComponents/navbar/navBar.tsx
+++ src/app/components/storyBookComponents/navbar/navBar.tsx
@@ -18,25 +18,25 @@ export const Button = ({
     return (
         <nav className='navBar'>
 
-            <Link className={(pathname == "/" ? "active" : "")} href="/">
+            <Link className={(pathname === "/" ? "active" : "")} href="/">
                 <span className='navButton' id='navHome'>
                     Hem
                 </span>
             </Link>
 
-            <Link className={(pathname == "/pages/planning" ? "active" : "")} href="/pages/planning">
+            <Link className={(pathname === "/pages/planning" ? "active" : "")} href="/pages/planning">
                 <span className='navButton' id='navPlanning'>
                     Planering
                 </span>
             </Link>
 
-            <Link className={(pathname == "/pages/history" ? "active" : "")} href="/pages/history">
+            <Link className={(pathname === "/pages/history" ? "active" : "")} href="/pages/history">
                 <span className='navButton' id='navHistory'>
                     Historik
                 </span>
             </Link>
 
-            <Link className={(pathname == "/pages/profile" ? "active" : "")} href="/pages/profile">
+            <Link className={(pathname === "/pages/profile" ? "active" : "")} href="/pages/profile">
                 <span className='navButton' id='navProfile'>
                     Profil
                 </span>
