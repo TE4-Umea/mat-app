25d397b5cb57f2f82b231d113e5409b90e4e33b0
Angelica Sjödin <angelic.sjodin@gmail.com>
2023-11-29 09:31:47 +0100

    🐛 mealtid

--- src/app/pages/food/page.tsx
+++ src/app/pages/food/page.tsx
@@ -6,14 +6,14 @@ export default function food() {
         
         <div className={styles.bg}>
             <h1 className={styles.title}>Kebabpitabred</h1>
-            <p className={styles.spara}>Lägg till i sparade</p>
+            <p className={styles.save}>Lägg till i sparade</p>
 
             <div className={styles.text1}><p>Sist du hade detta var</p></div>
             <div className={styles.text2}><p>November 34</p></div>
             <div className={styles.text11}><p>Du har ätit detta</p></div>
             <div className={styles.text22}><p>1 gång</p></div>
 
-            <div className={styles.måltid}><p>Måltidshistorik</p></div>
+            <div className={styles.mealtid}><p>Måltidshistorik</p></div>
 
             <div className={styles.bak1} id={styles.flex}>
                 <div className={styles.tex}><p>Måndag 13/11</p></div>
