18a7a3ff61d1c7987a95fb1521bb381be4065e25
timnilsson04 <timnils04@gmail.com>
2023-12-04 15:33:24 +0100

    Refactor setup page and remove unused code

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -1,11 +1,30 @@
 import styles from './page.module.css'
+// import { signIn, signOut } from 'next-auth/react';
+// import { getServerSession } from "next-auth";
 
 export default function Setup() {
+
+    // async function logIn() {
+    //     const session = await getServerSession();
+    //     if (session) {
+    //         return (
+    //             <main>
+    //                 <button onClick={() => signOut()}>Sign out</button>
+    //             </main>
+    //         )
+    //     } else if (!session) {
+    //         return (
+    //             <>
+    //                 <button onClick={() => signIn()}>Sign in</button>
+    //             </>
+    //         );
+    //     }
+    // }
     return (
         <>
             <h1 className={styles.h1}>Welcome to <span className={styles.span}>Storken</span>! We&apos;ll begin with configuring your profile.</h1>
 
-            <button></button> {/* Google auth button */}
+            {/* {logIn()} */}
 
             <h2 className={styles.h2}>Your preferred language</h2>
             <button></button> {/* Change to dropdown menu when initialized */}
