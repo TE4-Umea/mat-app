8612a55dab4030770ca72a15f35ea4979b52090f
timnilsson04 <timnils04@gmail.com>
2023-12-05 14:59:09 +0100

    Refactor historyCard component and add it to history page

--- src/app/pages/history/page.tsx
+++ src/app/pages/history/page.tsx
@@ -1,14 +1,19 @@
 import styles from './page.module.css'
+import { Card as HistoryCard } from '../../components/storyBookComponents/cards/history/historyCard'
 
 export default function History() {
     return (
+        <main>
 
-        <div className={styles.prison}>
-        
-        <h1 className={styles.title}>Historik</h1>
-        
-        <p className={styles.undertext}>Måltider du har haft</p>
-        </div>
-        
+            <div className={styles.prison}>
+
+                <h1 className={styles.title}>Historik</h1>
+
+                <p className={styles.undertext}>Måltider du har haft</p>
+            </div>
+
+            <HistoryCard></HistoryCard>
+
+        </main>
     )
 }
