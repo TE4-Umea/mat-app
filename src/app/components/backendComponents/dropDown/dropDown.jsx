4c6084de126560693e379de2923eaf5356d06437
= <=>
2023-12-07 10:39:19 +0100

    Refactor dropDown component

--- src/app/components/backendComponents/dropDown/dropDown.jsx
+++ src/app/components/backendComponents/dropDown/dropDown.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
 import styles from './dropDown.module.css';
-import Fetch from '../../../lib/fetch';
+import Fetch from '@/app/lib/fetch';
 import { useMealContext } from './MealContext';
 
 export default function DropDown({ mealType }) {
   const { selectedLunch, selectedDinner, setSelectedLunch, setSelectedDinner } = useMealContext();
   const [meals, setMeals] = useState([]);
   const [localSelectedMeal, setLocalSelectedMeal] = useState(null);
-
+  
   useEffect(() => {
     const fetchMealsData = async () => {
       try {
@@ -38,6 +38,11 @@ export default function DropDown({ mealType }) {
     setLocalSelectedMeal(randomMeal);
   };
 
+  const handleRemoveMeal = () => {
+    setLocalSelectedMeal(null);
+    localStorage.removeItem(mealType);
+  };
+
   useEffect(() => {
     let DeadSelect = false;
 
@@ -64,7 +69,7 @@ export default function DropDown({ mealType }) {
             {localSelectedMeal ? localSelectedMeal : '+ Lägg till måltid'}
           </Button>
         </DropdownTrigger>
-        <DropdownMenu className={styles.dropDownMenu}>
+        <DropdownMenu className={styles.dropDownMenu} aria-label={`Add ${mealType} Meal`}>
           {meals.map((meal, index) => (
             <DropdownItem key={index} onClick={() => handleMealSelect(meal)}>
               {meal}
@@ -76,14 +81,16 @@ export default function DropDown({ mealType }) {
         </DropdownMenu>
       </Dropdown>
 
-      <Button variant="contained" onClick={handleGenerateRandomMeal}>
-        Generera slumpad måltid
-      </Button>
+      {!localSelectedMeal && (
+        <Button variant="contained" onClick={handleGenerateRandomMeal} aria-label={`Add ${mealType} Meal`}>
+          Generera slumpad måltid
+        </Button>
+      )}
 
       {localSelectedMeal && (
-        <div>
-          <p>{mealType}: {localSelectedMeal}</p>
-        </div>
+        <Button variant="contained" onClick={handleRemoveMeal} aria-label={`Add ${mealType} Meal`}>
+          Remove
+        </Button>
       )}
     </div>
   );
