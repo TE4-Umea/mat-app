8f20621b71116c60e2e83efabc34ca89f72a0819
timnilsson04 <timnils04@gmail.com>
2023-12-11 09:29:04 +0100

    Add profile layout and update profile page

--- src/app/pages/profile/page.tsx
+++ src/app/pages/profile/page.tsx
@@ -1,14 +1,10 @@
-import ProfileClient from '@/app/components/storyBookComponents/cards/Profile/ProfileClient'
-import { getServerSession } from "next-auth";
-import { Profile } from '@/app/components/storyBookComponents/cards/Profile/Profile'
+
 
 export default async function ProfilePage() {
-    const session = await getServerSession();
     return (
-        <main>
-            <Profile></Profile>
-            <ProfileClient session={session} />
-        </main>
+        <>
+
+        </>
     )
 }
 
