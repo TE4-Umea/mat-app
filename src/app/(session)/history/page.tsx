f573259b1e4df350d9565e7edc0bd8b9e1711b38
timnilsson04 <timnils04@gmail.com>
2023-12-14 14:18:58 +0100

    Add meal provider and dynamic history cards

--- src/app/(session)/history/page.tsx
+++ src/app/(session)/history/page.tsx
@@ -1,20 +1,40 @@
+'use client'
 import styles from './page.module.css'
 import { HistoryCard } from '../../components/storyBookComponents/cards/history/historyCard'
+import { MealProvider } from '../../components/backendComponents/dropDown/MealContext';
 
 export default function History() {
+    const date = new Date();
+    const today = date.getMonth() + 1;
+    const todayaswell = date.getDate();
+    const data = [1, 2, 3, 4, 5, 6, 7]
+
+    const getDayOfWeek = (date, offset) => {
+        const newDate = new Date(date);
+        newDate.setDate(newDate.getDate() + offset);
+        return newDate.toLocaleString('default', { weekday: 'long' });
+    };
+
     return (
         <main>
+            <MealProvider currentDay={""}>
 
-            <div className={styles.prison}>
+                <div className={styles.prison}>
 
-                <h1 className={styles.title}>Historik</h1>
+                    <h1 className={styles.title}>Historik</h1>
 
-                <p className={styles.undertext}>Måltider du har haft</p>
-            </div>
+                    <p className={styles.undertext}>Måltider du har haft</p>
+                </div>
 
-            <HistoryCard></HistoryCard>
-
-
+                {data.map((offset) => (
+                    <HistoryCard
+                        key={offset}
+                        today={today}
+                        todayaswell={todayaswell + offset - 1}
+                        currentDay={getDayOfWeek(date, offset - 1)}
+                    ></HistoryCard>
+                ))}
+            </MealProvider>
         </main>
     )
 }
