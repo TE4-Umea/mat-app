cca7c81ce57dc1901ef5cf192e7f923a0be7cff1
timnilsson04 <timnils04@gmail.com>
2023-12-14 14:22:46 +0100

    Refactor HistoryCard component

--- src/app/components/storyBookComponents/cards/history/historyCard.tsx
+++ src/app/components/storyBookComponents/cards/history/historyCard.tsx
@@ -3,26 +3,26 @@ import React from 'react';
 import './historyCard.css';
 import '/src/app/globals.css';
 
-export function HistoryCard() {
+export function HistoryCard(props) {
   return (
     <>
-    <div className={['historyCard'].join(' ')}>
-      <div className='bakk'>
-        <p className='title'>Idag</p>
+      <div className={['historyCard'].join(' ')}>
+        <div className='bakk'>
+          <p className='title'>{props.todayaswell} / {props.today}</p>
 
-        {/* Display selected lunch */}
-        <p className='text'>Lunch</p>
-        <div className='ruta'>
-          <p className='textruta'>{ localStorage.getItem('Monday_Lunch')|| 'Ingen lunch vald'}</p>
-        </div>
+          {/* Display selected lunch */}
+          <p className='text'>Lunch</p>
+          <div className='ruta'>
+            <p className='textruta'>{localStorage.getItem('Monday_Lunch') || 'Ingen lunch vald'}</p>
+          </div>
 
-        {/* Display selected dinner */}
-        <p className='text'>Middag</p>
-        <div className='ruta'>
-          <p className='textruta'>{ localStorage.getItem('Monday_Dinner') || 'Ingen middag vald'}</p>
+          {/* Display selected dinner */}
+          <p className='text'>Middag</p>
+          <div className='ruta'>
+            <p className='textruta'>{localStorage.getItem('Monday_Dinner') || 'Ingen middag vald'}</p>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
