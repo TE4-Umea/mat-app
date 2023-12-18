824590f647a2343e4db4f3be2739faeb3287aac7
timnilsson04 <timnils04@gmail.com>
2023-12-18 09:34:52 +0100

    Update lunch and dinner display in HistoryCard component

--- src/app/[lng]/components/storyBookComponents/history/historyCard.tsx
+++ src/app/[lng]/components/storyBookComponents/history/historyCard.tsx
@@ -13,13 +13,13 @@ export function HistoryCard(props) {
           {/* Display selected lunch */}
           <p className='text'>Lunch</p>
           <div className='ruta'>
-            <p className='textruta'>{localStorage.getItem('Monday_Lunch') || 'Ingen lunch vald'}</p>
+            <p className='textruta'>{localStorage.getItem(`${props.currentDay}_Lunch`) || 'Ingen lunch vald'}</p>
           </div>
 
           {/* Display selected dinner */}
           <p className='text'>Middag</p>
           <div className='ruta'>
-            <p className='textruta'>{localStorage.getItem('Monday_Dinner') || 'Ingen middag vald'}</p>
+            <p className='textruta'>{localStorage.getItem(`${props.currentDay}_Dinner`) || 'Ingen middag vald'}</p>
           </div>
         </div>
       </div>
