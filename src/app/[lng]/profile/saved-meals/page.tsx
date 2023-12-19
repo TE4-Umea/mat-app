131b22afe30a6fd8da33d610971b91d083341e18
= <=>
2023-12-19 08:34:32 +0100

    Refactor saved meals page to fetch and display data, and add delete functionality

--- src/app/[lng]/profile/saved-meals/page.tsx
+++ src/app/[lng]/profile/saved-meals/page.tsx
@@ -4,27 +4,36 @@ export default async function SavedMeals({ params: { lng } }) {
     const { t } = await useTranslation(lng, ['glossary', 'common'])
 
     const List = async () => {
-        const response = await fetch (`http://jupiter.umea-ntig.se:3008/api/saved` , {
-          method: 'GET',
-          headers: {
-            authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJsdW5kbWFya2hqYWxtYXJAZ21haWwuY29tIiwiaWF0IjoxNzAyNDU1MTAzfQ.O9LhDq-P1jFVwDlToU8p_VUrRjsqQ60R1bybCa0B9yI',
-          },
+        const response = await fetch(`http://jupiter.umea-ntig.se:3008/api/saved`, {
+            method: 'GET',
+            headers: {
+                authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJsdW5kbWFya2hqYWxtYXJAZ21haWwuY29tIiwiaWF0IjoxNzAyNDU1MTAzfQ.O9LhDq-P1jFVwDlToU8p_VUrRjsqQ60R1bybCa0B9yI',
+            },
         });
         const data = await response.json();
         console.log(data);
         return data;
     };
+    const data = await List();
 
-    const data = await List(); 
+    const Delete = (id) => {
+        fetch(`http://jupiter.umea-ntig.se:3008/api/saved/${id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJsdW5kbWFya2hqYWxtYXJAZ21haWwuY29tIiwiaWF0IjoxNzAyNDU1MTAzfQ.O9LhDq-P1jFVwDlToU8p_VUrRjsqQ60R1bybCa0B9yI',
+            },
+        })
+    };
 
     return (
         <>
             <h1>{t('glossary:saved_button')}</h1>
-            <p>en potatis</p>
             {data.map((item) => (
                 <div key={item.id}>
                     <h1>{item.dish.name}</h1>
-                    <button>taborticon</button>
+                    <p id='remove'>{item.id}</p>
+                    <button>Radera</button>
+
                 </div>
             ))}
         </>
