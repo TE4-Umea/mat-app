f95cd8124dab7e071531e4fd586b27318bac0e18
timnilsson04 <timnils04@gmail.com>
2023-12-20 10:15:37 +0100

    Refactor greeting logic in GreetingCard component

--- src/app/components/storyBookComponents/greetingCard/greetingCard.tsx
+++ src/app/components/storyBookComponents/greetingCard/greetingCard.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './greetingCard.css';
-import '/src/app/globals.css';
-import { Time } from '../../backendComponents/timeDepender/timeDepender'
 import { getServerSession } from 'next-auth'
 import { useTranslation } from '../../../i18n'
 
@@ -9,9 +7,25 @@ export const GreetingCard = async ({ lng, dark }) => {
     const { t } = await useTranslation(lng, ['glossary', 'common'])
     const mode = dark ? 'dark' : 'light';
     const session = await getServerSession();
+    const today = new Date();
+    const currentHour = today.getHours();
+    let greeting;
+
+    if (currentHour >= 6 && currentHour < 12) {
+        greeting = t('glossary:greeting.morning');
+    } else if (currentHour >= 12 && currentHour < 18) {
+        greeting = t('glossary:greeting.afternoon');
+    } else {
+        greeting = t('glossary:greeting.evening');
+    }
+
     return (
-        <div className={['greetingCard', mode].join(' ')} >
-            <h1><Time lng={lng}></Time><span>{session?.user?.name}</span>! {t('glossary:eat.today')}</h1>
+        <div className={['greetingCard', mode].join(' ')}>
+            <h1>
+                {greeting}
+                <span>{session?.user?.name}</span>!
+            </h1>
+            <p>{t('glossary:eat.today')}</p>
         </div>
-    )
-}
+    );
+};
