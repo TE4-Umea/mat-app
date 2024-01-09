06cabbafb7e9e7ee2275869c203126c57c2f8260
timnilsson04 <timnils04@gmail.com>
2024-01-09 10:58:43 +0100

    Commented out unused imports and components in profile layout

--- src/app/[lng]/(loggedIn)/profile/layout.tsx
+++ src/app/[lng]/(loggedIn)/profile/layout.tsx
@@ -1,14 +1,14 @@
-import { dir } from 'i18next'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { redirect } from 'next/navigation'
-import { getServerSession } from 'next-auth'
-import { useTranslation } from 'next-i18next'
+// import { dir } from 'i18next'
+// import type { Metadata } from 'next'
+// import { Inter } from 'next/font/google'
+// import { redirect } from 'next/navigation'
+// import { getServerSession } from 'next-auth'
+// import { useTranslation } from 'next-i18next'
 
-import { NavBar } from '../../../components/storyBookComponents/navbar/navBar'
+// import { NavBar } from '../../../components/storyBookComponents/navbar/navBar'
+// import ProfileClient from '../../../components/storyBookComponents/profile/profileClient'
 import { NavProfile } from '../../../components/storyBookComponents/navProfile/navProfile'
 import { Profile } from '../../../components/storyBookComponents/profile/profile'
-import ProfileClient from '../../../components/storyBookComponents/profile/profileClient'
 import { languages } from '../../../i18n/settings'
 
 export async function generateStaticParams() {
