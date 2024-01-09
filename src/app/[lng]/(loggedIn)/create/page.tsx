966391b6aef972968d11c4e38237e725bdf29986
timnilsson04 <timnils04@gmail.com>
2024-01-09 09:04:07 +0100

    Commented out unused imports and translation code

--- src/app/[lng]/(loggedIn)/create/page.tsx
+++ src/app/[lng]/(loggedIn)/create/page.tsx
@@ -1,11 +1,11 @@
-import CreateButton from '@/app/components/backendComponents/createButtons/createButton'
-import DeleteButton from '@/app/components/backendComponents/createButtons/deleteButton'
-import { useTranslation } from '@/app/i18n'
+// import CreateButton from '@/app/components/backendComponents/createButtons/createButton'
+// import DeleteButton from '@/app/components/backendComponents/createButtons/deleteButton'
+// import { useTranslation } from '@/app/i18n'
 
 import Form from './form'
 
 export default async function History({ params: { lng } }) {
-    const { t } = await useTranslation(lng, ['glossary', 'common'])
+    // const { t } = await useTranslation(lng, ['glossary', 'common'])
 
     return (
         <main>
